491ed6001cf59699928781930305dc9cfcf6f5ce
Yunkun Lu <ricky@clockwork.io>
2023-12-21 14:50:00 -0800

    updated numbers go brrrr

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive, ref, watch } from 'vue';
+import { reactive, ref, watch } from "vue";
 class BufferQueue {
   name: string;
   size = 0;
@@ -9,13 +9,15 @@ class BufferQueue {
   next: BufferQueue | null;
   income?: number;
   drainCounter = 0;
+  stopReceiveThreshold: number;
   constructor(
     name: string,
     drainSize: number,
     drainSpeed: number,
     drainThreshold: number,
     next: BufferQueue | null,
-    income?: number
+    stopReceiveThreshold = 0,
+    income = 0
   ) {
     this.name = name;
     this.drainSize = drainSize;
@@ -23,6 +25,7 @@ class BufferQueue {
     this.drainThreshold = drainThreshold;
     this.next = next;
     this.income = income;
+    this.stopReceiveThreshold = stopReceiveThreshold;
   }
 
   update() {
@@ -34,18 +37,35 @@ class BufferQueue {
       return;
     }
     if (this.size >= this.drainThreshold) {
-      this.size -= this.drainSpeed;
-      this.next?.receive(this.drainSpeed);
-      this.drainCounter = this.drainSize / this.drainSpeed - 1;
+      if (this.next == null) {
+        this.size -= this.drainSpeed;
+        return;
+      }
+      if (this.next.canReceive()) {
+        this.size -= this.drainSpeed;
+        this.next?.receive(this.drainSpeed);
+        this.drainCounter = this.drainSize / this.drainSpeed - 1;
+      } else {
+        if (this.next.name === "rx_queue") {
+          // Special case, drop this packet
+          this.size -= PACKET_SIZE;
+        }
+      }
     }
+  }
+
+  canReceive() {
+    return (
+      this.stopReceiveThreshold === 0 || this.size < this.stopReceiveThreshold
+    );
   }
   receive(size: number) {
     this.size += size;
   }
 }
 
-const JUMBO_FRAME_SIZE = 16;
-const PACKET_SIZE = 32;
+const JUMBO_CHUNK_SIZE = 4;
+const PACKET_SIZE = 24;
 
 // Rules: bq1 receives 1 frame per second;
 // 1 sends 1 jumbo frame every 16 seconds
@@ -55,38 +75,81 @@ const PACKET_SIZE = 32;
 // 1 receives 1 frame per tick
 // 1: drainSize =drainSpeed = drainThreshold= JUMBO_FRAME_SIZE,
 
-const bq3 = reactive(new BufferQueue('bq3', PACKET_SIZE, 1, PACKET_SIZE, null));
-const bq2 = reactive(
-  new BufferQueue('bq2', PACKET_SIZE, JUMBO_FRAME_SIZE, PACKET_SIZE, bq3)
-);
-const bq1 = reactive(
+const txQueue = reactive(new BufferQueue("tx_queue", 1, 1, 1, null));
+const nfAxis = reactive(
   new BufferQueue(
-    'bq1',
-    JUMBO_FRAME_SIZE,
-    JUMBO_FRAME_SIZE,
-    JUMBO_FRAME_SIZE,
-    bq2,
-    1
+    "nf_axis_converter_main",
+    JUMBO_CHUNK_SIZE,
+    JUMBO_CHUNK_SIZE,
+    JUMBO_CHUNK_SIZE,
+    txQueue
+  )
+);
+const axisFifo = reactive(
+  new BufferQueue(
+    "axis_fifo",
+    JUMBO_CHUNK_SIZE,
+    JUMBO_CHUNK_SIZE,
+    JUMBO_CHUNK_SIZE,
+    nfAxis,
+    60
   )
 );
 
-const bqs = ref([bq1, bq2, bq3]);
+const nicOutputLookup = reactive(
+  new BufferQueue(
+    "nic_output_port_lookup",
+    JUMBO_CHUNK_SIZE,
+    JUMBO_CHUNK_SIZE,
+    JUMBO_CHUNK_SIZE,
+    axisFifo,
+    12
+  )
+);
 
-const numBqs = ref(3);
+const nicOutputQueue = reactive(
+  new BufferQueue(
+    "nic_output_queues",
+    JUMBO_CHUNK_SIZE,
+    JUMBO_CHUNK_SIZE,
+    JUMBO_CHUNK_SIZE,
+    nicOutputLookup,
+    312
+  )
+);
 
-watch(numBqs, () => {
-  if (numBqs.value > bqs.value.length) {
-    for (let i = 0; i < numBqs.value - bqs.value.length; i += 1) {
-      const newBq = reactive(
-        new BufferQueue(`bq ${bqs.value.length + 1}`, 1, 1, 1, null)
-      );
-      bqs.value[bqs.value.length - 1].next = newBq;
-      bqs.value.push(newBq);
-    }
-  } else if (numBqs.value < bqs.value.length) {
-    bqs.value.length = numBqs.value;
-  }
-});
+const inputArbiter = reactive(
+  new BufferQueue("input_arbiter", 4, 4, 4, nicOutputQueue, 252)
+);
+
+const nfAxisFirst = reactive(
+  new BufferQueue("nf_axis_converter_main(0)", 4, 4, 4, inputArbiter, 63)
+);
+
+const axisFifoFirst = reactive(
+  new BufferQueue("axis_fifo(0)", 1, 1, 1, nfAxisFirst, 63)
+);
+
+const rxQueue = reactive(
+  new BufferQueue("rx_queue", 1, 1, 1, axisFifoFirst, 331)
+);
+
+const inputQueue = reactive(
+  new BufferQueue("axi_10g_ethernet_shared", 1, 1, 1, rxQueue, 0, 1)
+);
+
+const bqs = ref([
+  inputQueue,
+  rxQueue,
+  axisFifoFirst,
+  nfAxisFirst,
+  inputArbiter,
+  nicOutputQueue,
+  nicOutputLookup,
+  axisFifo,
+  nfAxis,
+  txQueue,
+]);
 
 function simulate() {
   bqs.value.forEach((bq) => {
@@ -96,7 +159,7 @@ function simulate() {
 
 const simSpeed = ref(100);
 const simStarted = ref(false);
-let simInterval;
+let simInterval: number;
 
 function startSim() {
   simStarted.value = true;
@@ -115,10 +178,6 @@ function endSim() {
     <button v-else @click="endSim">End sim</button>
     <div>Sim Interval | lower number the faster</div>
     <input v-model="simSpeed" type="number" />
-    <div class="my-2 row d-flex justify-content-center">
-      <button @click="numBqs += 1">add BQ</button>
-      <button @click="numBqs = Math.max(1, numBqs - 1)">remove BQ</button>
-    </div>
     <div class="d-flex row justify-content-center">
       <div v-for="bq in bqs" :key="bq.name" class="col-auto">
         <div>drainSize</div>
@@ -139,9 +198,8 @@ function endSim() {
   </div>
 </template>
 
-
 <style>
-.background  {
+.background {
   padding-top: 30px;
 }
 button {
@@ -157,6 +215,6 @@ input {
 
 .frame {
   height: 2px;
-  background-color: white
+  background-color: white;
 }
 </style>
