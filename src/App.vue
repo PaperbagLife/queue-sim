21ec5e7feae3aa7972b3954fa35dbcece7f29a32
Yunkun Lu <ricky@clockwork.io>
2024-01-08 10:19:55 -0800

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
-  <MapView />
+  <div class="col">
+    <router-link class="col-auto" to="/queue-sim">QueueSim</router-link>
+    <router-link class="col-auto" to="/map">Bandwidth Map</router-link>
+  </div>
+
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import MapView from "./components/MapView.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    MapView,
-  },
+  components: {},
 });
 </script>
 
@@ -23,5 +27,8 @@ export default defineComponent({
   color: #c8c8c8;
   background: rgb(22, 22, 22);
   min-height: 100vh;
+}
+router-link {
+  color: white;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import MapView from './components/MapView.vue';
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/queue-sim', component: HelloWorld},
+    { path: '/map', component: MapView},
+  ]
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+  })
+
+createApp(App).use(router).mount('#app')
